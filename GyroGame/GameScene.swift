232835e48a7062f9df44f144697974d1e9a20572
Emilian Brenici <brenici@gmail.com>
2021-10-03 13:22:03 +0200

    Add player to game scene, add motion manager

--- GyroGame/GameScene.swift
+++ GyroGame/GameScene.swift
@@ -10,15 +10,43 @@ import CoreMotion
 
 class GameScene: SKScene {
     
+    private var motionManager: CMMotionManager!
+    private var playerNode: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         loadScene()
+        startMotionManager()
     }
     
-    func loadScene() {
+    private func loadScene() {
+        physicsWorld.gravity = .zero
         addSceneBackground()
+        createPlayer()
     }
     
-    func addSceneBackground() {
+    private func startMotionManager() {
+        motionManager = CMMotionManager()
+        guard motionManager.isAccelerometerAvailable else {
+            return
+        }
+        motionManager.startAccelerometerUpdates()
+    }
+    
+    private func createPlayer() {
+        playerNode = SKSpriteNode(imageNamed: "ball")
+        playerNode.position = CGPoint(x: 555, y: -372)
+        playerNode.size = CGSize(width: 80, height: 80)
+        playerNode.physicsBody?.allowsRotation = false
+        playerNode.physicsBody?.linearDamping = 0.4
+        playerNode.physicsBody?.restitution = 0.2
+        playerNode.physicsBody?.categoryBitMask = 1
+        playerNode.physicsBody?.contactTestBitMask = 2 | 3 // holes and finish
+        playerNode.physicsBody?.collisionBitMask = 2 // walls
+        playerNode.zPosition = 1 // in front of bg and all holes
+        addChild(playerNode)
+    }
+    
+    private func addSceneBackground() {
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.zPosition = -1
         addChild(background)
